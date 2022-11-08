31598d5257a023cc7b619c5a80966b66b82123be
MRizwan7 <117256951+MRizwan7@users.noreply.github.com>
2022-11-07 20:56:57 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+     public static void GreatestOfNumbers(int a, int b, int c) {
+        if (a > b && a > c) {
+            System.out.println("Largest Number is " + a);
+        } else if (b > a && b > c) {
+            System.out.println("Largest Number is " + b);
+        } else {
+            System.out.println("Largest Number is " + c);
+        }
+    }
 }
